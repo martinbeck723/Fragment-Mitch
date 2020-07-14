4ecd2f416687dee1b6bf6bab1fb7d81e4e1a8d66
martinbeck723 <xishuoliu514@gmail.com>
2020-07-14 15:32:46 -0400

    adapter added

--- app/src/main/java/com/raywenderlich/fragments_mitch_java/SecondActivity.java
+++ app/src/main/java/com/raywenderlich/fragments_mitch_java/SecondActivity.java
@@ -1,16 +1,18 @@
 package com.raywenderlich.fragments_mitch_java;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "SecondActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity_layout);
-
+        Log.d(TAG, "onCreate: started.");
 
     }
 }
---
+++ app/src/main/java/com/raywenderlich/fragments_mitch_java/SectionsStatePagerAdapter.java
@@ -0,0 +1,24 @@
+package com.raywenderlich.fragments_mitch_java;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    public SectionsStatePagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
