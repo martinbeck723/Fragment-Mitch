b18a23feba23272a03a9938ae6a482c4bafe596b
martinbeck723 <xishuoliu514@gmail.com>
2020-07-15 10:55:43 -0400

    add frag1

---
+++ app/src/main/java/com/raywenderlich/fragments_mitch_java/Fragment1.java
@@ -0,0 +1,41 @@
+package com.raywenderlich.fragments_mitch_java;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    private static final String TAG = "Fragment1";
+
+    private Button btnNavFrag1;
+    private Button btnNavFrag2;
+    private Button btnNavFrag3;
+    private Button btnNavSecondActivity;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1_layout, container, false);
+        btnNavFrag1 = (Button) view.findViewById(R.id.btnNavFrag1);
+        btnNavFrag2 = (Button) view.findViewById(R.id.btnNavFrag2);
+        btnNavFrag3 = (Button) view.findViewById(R.id.btnNavFrag3);
+        btnNavSecondActivity = (Button) view.findViewById(R.id.btnNavSecondActivity);
+        Log.d(TAG, "onCreateView: started.");
+
+        btnNavFrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
+}
